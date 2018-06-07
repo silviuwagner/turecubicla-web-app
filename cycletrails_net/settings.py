5825f1c0fd93924167c13d6f44c8a591fc46f206
Paul Wagner <silviu.wagner@gmail.com>
2018-06-07 21:00:52 +0300

    Heroku configuration ready for deployment

--- cycletrails_net/settings.py
+++ cycletrails_net/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xd#+j%tj#+zc$+iswvw89jf^$5@fptrb!gnfqz#od&yf_q$)37'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
