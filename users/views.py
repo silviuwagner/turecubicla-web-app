89af7e9270ae202d115b7595b6559afc0aecc7a4
Paul Wagner <silviu.wagner@gmail.com>
2018-07-19 19:15:32 +0300

    user profile added

--- users/views.py
+++ users/views.py
@@ -5,15 +5,20 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views import generic
 from .forms import CustomUserCreationForm
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from . import models
+import trails.models
 
 class SignUp(generic.CreateView):
 	form_class = CustomUserCreationForm
 	success_url = reverse_lazy('login')
 	template_name = 'signup.html'
+
+class PublicUserProfile(TemplateView):
+	model = models.CustomUser
+	template_name = 'public_profile.html'
 
 class UserProfile(LoginRequiredMixin, TemplateView):
 	model = models.CustomUser
@@ -21,7 +26,12 @@ class UserProfile(LoginRequiredMixin, TemplateView):
 	fields = ['about', 'age', 'location', 'bike']
 	login_url = 'login'
 
-class PublicUserProfile(TemplateView):
-	model = models.CustomUser
-	template_name = 'public_profile.html'
+class UserProfileTrails(LoginRequiredMixin, ListView):
+	model = trails.models.Trail
+	template_name = 'user_profile_trails.html'
+	login_url = 'login'
+
+class UserProfileSettings(LoginRequiredMixin, TemplateView):
+	template_name = 'user_profile_settings.html'
+	login_url = 'login'
 	
