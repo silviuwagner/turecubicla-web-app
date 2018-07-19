783be21215eb629d2f99c4ad744c26e83c4a2b94
Paul Wagner <silviu.wagner@gmail.com>
2018-07-19 14:56:09 +0300

    some changes

--- trails/models.py
+++ trails/models.py
@@ -2,6 +2,10 @@ from django.conf import settings
 from django.db import models
 from django.urls import reverse
 from .validators import validate_file_extension
+# from PIL import Image
+# from io import BytesIO
+# from django.core.files.uploadedfile import InMemoryUploadedFile
+# import sys
 
 class Trail(models.Model):
 	title = models.CharField(max_length=255)
@@ -15,11 +19,10 @@ class Trail(models.Model):
 	region = models.CharField(max_length=100, default='Romania')
 	distance = models.PositiveIntegerField(default='0', verbose_name='Distance (in KM)')
 
-
-	EASY = "EASY"
-	MODERATE = "MODERATE"
-	HARD = "HARD"
-	EXTREME = "EXTREME"
+	EASY = "Easy"
+	MODERATE = "Moderate"
+	HARD = "Hard"
+	EXTREME = "Extreme"
 
 	DIFFICULTY_CHOICES = (
 		(EASY, "Easy"),
@@ -27,12 +30,31 @@ class Trail(models.Model):
 		(HARD, "Hard"),
 		(EXTREME, "Extreme"),
 	)
+
 	difficulty = models.CharField(max_length=10, choices=DIFFICULTY_CHOICES)
 
 	homepage_staff_pick = models.BooleanField(default=0)
 
 	#image upload
 	image = models.ImageField(upload_to='image/', default='imagelink.jpg')
+	# def save(self):
+	# 	#Opening the uploaded image
+	# 	im = Image.open(self.image)
+
+	# 	output = BytesIO()
+
+	# 	#Resize/modify the image
+	# 	im = im.resize( (1920,1442) )
+
+	# 	#after modifications, save it to the output
+	# 	im.save(output, format='JPEG', quality=100)
+	# 	output.seek(0)
+
+	# 	#change the imagefield value to be the newley modifed image value
+	# 	self.image = InMemoryUploadedFile(output,'ImageField', "%s.jpg" %self.image.name.split('.')[0], 'image/jpeg', sys.getsizeof(output), None)
+
+	# 	super(Trail,self).save()
+
 	image_uploaded_at = models.DateTimeField(auto_now_add=True)
 
 	#kml/gpx
